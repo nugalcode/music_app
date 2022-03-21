e16a6e23a432a15adf176507bc8d54545d0ee86d
Jed Nugal <nugaljed@gmail.com>
2022-03-21 15:54:03 -0700

    Clicking on the playIcon in YourLibrary plays the playlist
    It plays the playlist while keeping the display in Your Library, similar to the way Spotify behaves. It only changes to display the playlist tracks if it is clicked inside the playlistPreview, but outside the playIcon

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -144,31 +144,11 @@ export const Dashboard = ({ code }) => {
     }, [currentPlaylist, accessToken])
 
     const changeUrisByPlaylist = (playlist) => {
+        console.log("Inside changeUrisByPlaylist");
         spotifyApi.getPlaylistTracks(playlist.playlistID).then(res => {
             setCurrentUris(
                 res.body.items.map((item, index) => {
-                    const track = item.track;
-                    // find the smallest album image
-                    const smallestAlbumImage = track.album.images.reduce(
-                        (smallest, image) => {
-                            if (image.height < smallest.height) return image
-                            return smallest;
-                        },
-                        track.album.images[0]
-                    )
-
-                    const duration = convertDuration(track.duration_ms);
-
-                    return {
-                        artist: track.artists[0].name,
-                        title: track.name,
-                        uri: track.uri,
-                        albumName: track.album.name,
-                        albumUrl: smallestAlbumImage.url,
-                        duration: duration,
-                        offset: index,
-                        id: track.id,
-                    }
+                    return item.track.uri
                 })
             )
         }).catch(() => {
--- src/components/YourLibrary.js
+++ src/components/YourLibrary.js
@@ -4,9 +4,16 @@ import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 const YourLibrary = ({ playlists, handlePlaylistTracks, changeUrisByPlaylist }) => {
 
-    const handleOnClick = (playlist) => {
+    const handlePlaylist = (playlist) => {
         handlePlaylistTracks(playlist)
     }
+
+    const handleUris = (e, playlist) => {
+        // prevents onClick event from bubbling up and triggering the parent onClick event
+        e.stopPropagation();
+        changeUrisByPlaylist(playlist);
+    }
+
     return (
         <div className="yourLibrary">
             <div className="playlistsPreviewContainer">
@@ -17,11 +24,13 @@ const YourLibrary = ({ playlists, handlePlaylistTracks, changeUrisByPlaylist }) 
                         return (
                             <div className="playlistPreview"
                                 key={index}
-                                onClick={() => handleOnClick(playlist)}
+                                onClick={() => handlePlaylist(playlist)}
                             >
                                 <div className="imgWrap"> 
                                     <img className="playlistImage" src={playlist.image !== "" ? playlist.image : "https://assets.dryicons.com/uploads/icon/svg/8256/d635dcbf-b76c-420b-a334-23eac4052ada.svg"} alt="" />
-                                    <div className="playIconWrap"> <PlayCircleIcon className="playCircleIcon"/> </div>
+                                    <div className="playIconWrap">
+                                        <PlayCircleIcon className="playCircleIcon" onClick={(e) => handleUris(e, playlist)} />
+                                    </div>
                                 </div>
                                 <span> {playlist.name} </span>
                             </div>
