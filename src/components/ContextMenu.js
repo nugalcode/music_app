cb4e6291b98fef10e4090e9a9567dd09ddd00610
Jed Nugal <nugaljed@gmail.com>
2022-04-11 11:33:50 -0700

    Added playlist display when hovering on Add to playlist option in ContextMenu

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useState } from 'react';
 import '../css/ContextMenu.css';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const ContextMenu = ({ position, playlists }) => {
 	const style = {
@@ -7,11 +8,41 @@ const ContextMenu = ({ position, playlists }) => {
 		left: position.x,
 		position: 'absolute'
 	}
+
+	const [addPlaylistHover, setAddPlaylistHover] = useState(false)
+	
+	const handleOnMouseOver = () => {
+		setAddPlaylistHover(true);
+	}
+	const handleOnMouseLeave = () => {
+		setAddPlaylistHover(false);
+	}
+	const handlePreventBubble = (e) => {
+		e.stopPropagation();
+    }
 	return (
 		<div className="contextMenu" style={style}>
-			<div className="contextMenuItem"> Saved to your Liked Songs </div>
-			<div className="contextMenuItem"> Add to playlist </div>
-			<div className="contextMenuItem"> Option 3 </div>
+			<div className="contextMenuItem"
+				onMouseEnter={() => handleOnMouseOver()}
+				onMouseLeave={() => handleOnMouseLeave()}>
+				Add to playlist
+				<ArrowRightIcon />
+				{addPlaylistHover &&
+					<div className="contextPlaylists"
+						onMouseDown={(e) => handlePreventBubble(e)}>
+						{playlists.map((playlist, index) => {
+							return (
+								<div className="contextMenuItem" key={index}>
+									{playlist.name}
+								</div>
+							)
+						})}
+					</div>
+				}
+			</div>
+			
+			<div className="contextMenuItem"> Placeholder </div>
+			<div className="contextMenuItem"> Placeholder </div>
 		</div>
 	)
 }
