d500aa0bddd901be9bab1432c034ead6ea4bdb76
Jed Nugal <nugaljed@gmail.com>
2022-04-11 14:08:06 -0700

    Removed unused code and console.logs in playDetailsContext

--- src/hooks/playDetailsContext.js
+++ src/hooks/playDetailsContext.js
@@ -19,14 +19,10 @@ export const ACTIONS = {
 const reducer = (state, action) => {
     switch (action.type) {
         case "changeUris":
-            console.log("changeUris action")
-            console.log(action.offset);
             return { ...state, uris: action.uris, offset: action.offset };
         case "startPlaying":
-            console.log("startPlaying")
             return { ...state, play: true };
         case "stopPlaying":
-            console.log("stopPlaying");
             return { ...state, uris: [], play: false };
         default:
             throw new Error();
@@ -36,11 +32,6 @@ const reducer = (state, action) => {
 
 export const PlayDetailsProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
-    // use memo prevents unnecessary rerenders of children who are subscribed to this context
-   /* const contextValue = useMemo(() => {
-        return { state, dispatch };
-    }, [state, dispatch])*/
 
     return (
         <playDetailsStateContext.Provider value={state}>
