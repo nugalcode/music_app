ec7d29e8612c90d6f2d9f74884c4a5392b5e3fd7
Jed Nugal <nugaljed@gmail.com>
2022-04-13 15:50:00 -0700

    Updated Login css

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     return (
         <div className="login">
             <a className="loginButton" href={AUTH_URL}>
-                Spotify Login
+                <span> Spotify Login </span>
             </a>
         </div>
     )
