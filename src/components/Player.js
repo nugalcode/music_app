63ec820b557d45de4a7cf6fceef7e5bd6ac2cd0c
Jed Nugal <nugaljed@gmail.com>
2022-04-13 16:16:12 -0700

    Deleted unused code in Player.js
    Also deleted showSaveIcon property of SpotifyPlayer to get rid of the heart icon

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,33 +1,7 @@
 import { useContext, useEffect } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback'
 import { ACTIONS, playDetailsDispatchContext, playDetailsStateContext } from "../hooks/playDetailsContext";
-/*const initialState =
-{
-    uris: [],
-    offset: 0,
-    play: false,
-}
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "changeUris":
-            console.log("changeUris")
-            return { uris: action.uris, offset: action.offset, play: true };
-        case "startPlaying":
-            console.log("startPlaying")
-            console.log(state.offset);
-            return { uris: [...state.uris], offset: state.offset, play: true };
-        case "stopPlaying":
-            console.log("stopPlaying");
-            return { uris: action.uris, offset: action.offset, play: false };
-        case "stopPlayingFromTrack":
-            console.log("stopPlaying");
-            return { uris: [...state.uris], offset: state.offset, play: false };
-     
-        default:
-            throw new Error();
-    }
-}*/
 export default function Player({ accessToken }) {
     const dispatch = useContext(playDetailsDispatchContext);
     const  playDetailsState  = useContext(playDetailsStateContext);
@@ -54,7 +28,6 @@ export default function Player({ accessToken }) {
 
     return <SpotifyPlayer
         token={accessToken}
-        showSaveIcon
         callback={state => {
             if (!state.isPlaying) {
                 handleStopPlayingCallback();
